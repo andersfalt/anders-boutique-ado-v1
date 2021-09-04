a5fc4b891baff4fbc899a5aebe757b4871e09692
andersfalt <andersfalt1@gmail.com>
2021-09-04 18:04:54 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JW2ytIOxJFy7n0LIOhhfyBeSaaSUOKj07rrHYcY5lR9AKNpwrV1iMWY1Y0s5DtVUJNJapdKw37f9O9qHSyqDHjU002eWG4Am8',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
